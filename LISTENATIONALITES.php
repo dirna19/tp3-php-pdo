d6f4faa081e6987c828db7b0bb00b778573904d3
dirna19 <laposteps3@gmail.com>
2026-02-05 12:09:04 +0100

    video 3

---
+++ LISTENATIONALITES.php
@@ -0,0 +1,90 @@
+
+<?php include "header.php";
+include "connexion-pdo.php";
+$req=$monPdo->prepare("select * from nationalite");
+$req->setFetchMode(PDO::FETCH_OBJ);
+$req->execute();
+$listenationalitees=$req->fetchALL();
+var_dump($listenationalitees);
+?>
+<main role="main">
+<div class="container">
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">numero</th>
+      <th scope="col">libellé</th>
+      <th scope="col">actions</th>
+    
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    foreach($listenationalitees as $nationalite)
+        ?>
+   echo "<tr>" class="table-active">
+      <th scope="row">Active</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr>
+      <th scope="row">Default</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-primary">
+      <th scope="row">Primary</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-secondary">
+      <th scope="row">Secondary</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-success">
+      <th scope="row">Success</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-danger">
+      <th scope="row">Danger</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-warning">
+      <th scope="row">Warning</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-info">
+      <th scope="row">Info</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-light">
+      <th scope="row">Light</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+    <tr class="table-dark">
+      <th scope="row">Dark</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+    </tr>
+  </tbody>
+</table>
+
+</main>
+<?php include "footer.php";
+?>
